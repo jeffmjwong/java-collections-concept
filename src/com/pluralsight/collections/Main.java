e04e7bcdfc64b42d0980c58d6466a67636e39869
Jeff Wong <v-jwong1@expedia.com>
2019-12-27 21:49:09 +1000

    use Collection contains() method

--- src/com/pluralsight/collections/Main.java
+++ src/com/pluralsight/collections/Main.java
@@ -32,5 +32,7 @@ public class Main {
         System.out.println(products);
         System.out.println(products.size());
         System.out.println(products.isEmpty());
+        System.out.println(products.contains(window));
+        System.out.println(products.contains(door));
     }
 }
