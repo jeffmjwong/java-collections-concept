ebbfa7d0b0438748b42bd80d5e0cab4e52aff416
Jeff Wong <v-jwong1@expedia.com>
2019-12-27 20:52:46 +1000

    specify details for Product class

--- src/com/pluralsight/collections/Product.java
+++ src/com/pluralsight/collections/Product.java
@@ -1,4 +1,24 @@
 package com.pluralsight.collections;
 
 public class Product {
+    private String name;
+    private int weight;
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getWeight() {
+        return this.weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Product(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
 }
